206aa3e21cf287a1766c424e4d708353a9bea612
devankit1994 <devankit1994@gmail.com>
2025-07-10 20:42:33 +0530

    WIP

--- utils/contants.js
+++ utils/contants.js
@@ -1,2 +1,1 @@
-// export const result =  'https://www.ipmcareer.com/wp-content/uploads/2020/06/42-indore-.jpg'
-export const result = './INDORE-BANNER.jpg';
+export const result = "./INDORE-BANNER.jpg";
