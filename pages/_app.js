6f0bf324dc5981039f0782b14a3e3133a135b0b5
Vivekipm2802 <ipmcareeronline@gmail.com>
2026-04-09 16:34:20 +0530

    Add Google Tag AW-16670515724 for Google Ads conversion tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,37 @@
 import '../styles/globals.css'
-import {NextUIProvider} from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react"
+import Scrollbar from 'smooth-scrollbar-react'
+import Head from 'next/head'
+import { Toaster } from 'react-hot-toast'
+import Script from 'next/script'
 
-import { Scrollbar } from 'smooth-scrollbar-react';
-import Head from 'next/head';
-import { Toaster } from 'react-hot-toast';
 export default function App({ Component, pageProps }) {
-    return <>
-    <Head>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <link rel="icon" href={'/favicon_ipm.svg'} />
-
-
-    </Head>
-    <Toaster position="bottom-right" toastOptions={{className:'font-sans'}}></Toaster>
-    <Scrollbar
-    damping={1}
-    thumbMinSize={10}
-    >
-       <NextUIProvider>
-      <Component {...pageProps }/></NextUIProvider></Scrollbar></>
+  return (
+    <>
+      {/* Google tag (gtag.js) — Google Ads AW-16670515724 */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-16670515724"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16670515724');
+        `}
+      </Script>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <link rel="icon" href='/favicon_ipm.svg' />
+      </Head>
+      <Toaster position="bottom-right" toastOptions={{ className: 'font-sans' }} />
+      <Scrollbar damping thumbMinSize>
+        <NextUIProvider>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </Scrollbar>
+    </>
+  )
 }
