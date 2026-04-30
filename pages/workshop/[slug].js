88e651eec3b187949de565b7ee39aade434e23f5
ClatTribe <ctribeweb@gmail.com>
2026-04-30 14:45:28 +0530

    Fix build: replace @next/font/google with next/font/google across all pages

--- pages/workshop/[slug].js
+++ pages/workshop/[slug].js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import { Inter } from 'next/font/google'
 import {useState,useEffect} from 'react'
 import styles from './Workshop.module.css'
 import FAQ from '../../components/FAQ'
