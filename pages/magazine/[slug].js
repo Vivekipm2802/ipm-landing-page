3cede1020b2dc338a75a96debc4a0bd58caea6c0
ClatTribe <ctribeweb@gmail.com>
2026-05-05 21:50:10 +0530

    fix: magazine pages use service key to bypass RLS

--- pages/magazine/[slug].js
+++ pages/magazine/[slug].js
@@ -6,16 +6,13 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import { createClient } from '@supabase/supabase-js';
+import { getSupabaseServer } from '../../utils/supabaseClient';
 import IPMNav from '../../components/IPMNav';
 import BlogCard from '../../components/BlogCard';
 import { gradientCss, gradientFor } from '../../lib/gradients';
 
 export async function getServerSideProps({ params, res }) {
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  );
+  const supabase = getSupabaseServer();
   const { data: blog } = await supabase
     .from('blogs')
     .select('*')
