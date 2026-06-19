74a50b6350cb7ef074dc78de2368510e82865fb4
ClatTribe <ctribeweb@gmail.com>
2026-06-19 20:04:15 +0530

    GTM-PS6TC9MN: add to every page via _document.js, remove duplicate from new-delhi

--- pages/_document.js
+++ pages/_document.js
@@ -67,8 +67,15 @@ export default function Document() {
             />
           </noscript>
 
+
+      {/* Google Tag Manager - GTM-PS6TC9MN */}
+      <script dangerouslySetInnerHTML={{__html:"(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\"gtm.start\":new Date().getTime(),event:\"gtm.js\"});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!=\"dataLayer\"?'&l='+l:'';j.async=true;j.src=\"https://www.googletagmanager.com/gtm.js?id=\"+i+dl;f.parentNode.insertBefore(j,f);})(window,document,\"script\",\"dataLayer\",\"GTM-PS6TC9MN\");"}}></script>
+      {/* End Google Tag Manager - GTM-PS6TC9MN */}
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) - GTM-PS6TC9MN */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PS6TC9MN" height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) - GTM-PS6TC9MN */}
         
         <Main />
         <NextScript />
